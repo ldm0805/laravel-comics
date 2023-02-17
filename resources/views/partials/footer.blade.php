a14de4082a07d88688f61cdffb86b2e5c9a39785
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-02-17 09:30:12 +0100

    Aggiunta struttura copyright

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -13,62 +13,77 @@
    </div>
    
     <div class="cols cont_list">
-        <div>
-            <div class="footer-links">
-                <ul>
-                    <li><h4>Dc Comics</h4></li>
-                    <li><a href="#">Characters</a></li>
-                    <li><a href="#">Comics</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">TV</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">News</a></li>
-                </ul>
+        <div class="d-flex flex-column">
+            <div class="d-flex flex-row">
+                <div>
+                    <div class="footer-links">
+                        <ul>
+                            <li><h4>Dc Comics</h4></li>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                    </div>
+                    <!-- Link footer -->
+                    <div class="footer-links">
+                        <ul>
+                            <li><h4>Shop</h4></li>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Link footer -->
+                <div class="footer-links">
+                    <ul>
+                        <li><h4>DC</h4></li>
+                        <li><a href="#">Temos Of Use</a></li>
+                        <li><a href="#">Privacy policy (New)</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Cerrificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+            
+                <div class="footer-links">
+                    <ul>
+                        <li><h4>SITES</h4></li>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
             </div>
-            <!-- Link footer -->
-            <div class="footer-links">
-                <ul>
-                    <li><h4>Shop</h4></li>
-                    <li><a href="#">Shop DC</a></li>
-                    <li><a href="#">Shop DC Collectibles</a></li>
-                </ul>
-            </div>
+                <div>
+                    <p class="copyright">
+                        All Site Content and 
+                        <i class="fa-regular fa-copyright"></i>
+                        DC Entertaiment, unless otherwise 
+                        <a href="#">noted here</a>.
+                        All rights reserved.
+                        <a href="#">Cookies Settings</a>
+                    </p>
+                </div>
         </div>
-        <!-- Link footer -->
-        <div class="footer-links">
-            <ul>
-                <li><h4>DC</h4></li>
-                <li><a href="#">Temos Of Use</a></li>
-                <li><a href="#">Privacy policy (New)</a></li>
-                <li><a href="#">Ad Choices</a></li>
-                <li><a href="#">Advertising</a></li>
-                <li><a href="#">Jobs</a></li>
-                <li><a href="#">Subscriptions</a></li>
-                <li><a href="#">Talent Workshops</a></li>
-                <li><a href="#">CPSC Cerrificates</a></li>
-                <li><a href="#">Ratings</a></li>
-                <li><a href="#">Shop Help</a></li>
-                <li><a href="#">Contact Us</a></li>
-            </ul>
-        </div>
-
-        <div class="footer-links">
-            <ul>
-                <li><h4>SITES</h4></li>
-                <li><a href="#">DC</a></li>
-                <li><a href="#">MAD Magazine</a></li>
-                <li><a href="#">DC Kids</a></li>
-                <li><a href="#">DC Universe</a></li>
-                <li><a href="#">DC Power Visa</a></li>
-            </ul>
-        </div>
-
+        
+        
+    
         <div class="dc_bg">
             <div class="img_dc_bg">
             </div>
         </div>
-
     </div>
     <div class="social">
         <div class="col-but">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,26 +1,33 @@
 <header>
-    <div>
-        <img src="{{Vite::asset('resources/images/dc-logo.png')}}" alt="">
-
+    <div class="visa-cont">
+        <div class="visa">
+            <p>Dc Power Visa</p>
+            <p>Additional Dc Sites<i class="fa-regular fa-registered ps-1"></i></p>
+        </div>
     </div>
-    <div>
-    <nav class="nav_cont">
-        <ul>
-            @foreach($productsmenu as $menuitem)
-            <li>
-                <a
+    <section>
+        <div>
+            <img src="{{Vite::asset('resources/images/dc-logo.png')}}" alt="">
+        </div>
+        <div>
+            <nav class="nav_cont">
+                <ul>
+                    @foreach($productsmenu as $menuitem)
+                    <li>
+                        <a
                 class="{{Route::currentRouteName() == 'homepage' ? 'active' : ''}}" 
-                 href="{{$menuitem['url']}}">{{$menuitem['label']}}
-                </a>
-            </li>
-            @endforeach
-        </ul>
+                href="{{$menuitem['url']}}">{{$menuitem['label']}}
+            </a>
+        </li>
+        @endforeach
+    </ul>
     
+</nav>
+</div>
+<div class="search">
+    <nav class="input-cont">
+        <input type="text" placeholder="Type something...">
     </nav>
-    </div>
-    <div class="search">
-        <nav class="input-cont">
-            <input type="text" placeholder="Type something...">
-          </nav>
-    </div>
+</div>
+</section>
 </header>
