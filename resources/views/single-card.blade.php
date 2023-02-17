770ab2f448285614a2c6c017218f7a55597032b5
Leonardo Di Maggio <leonardodimaggio94@gmail.com>
2023-02-17 20:38:20 +0100

    Modifiche single card: -position absolute fixed; -aggiunte scritte sulla copertina.

--- resources/views/single-card.blade.php
+++ resources/views/single-card.blade.php
@@ -2,43 +2,47 @@
 @section('content')
 <div class="single-card">
     <div class="position">
-        <img class="thumb" src="{{$single['thumb']}}" alt="{{$single['title']}}">
+        <div class="view">
+            <img class="thumb" src="{{$single['thumb']}}" alt="{{$single['title']}}">
+            <div>View Gallery</div>
+            <div class="comic">{{$single['type']}}</div>
+        </div>
     </div>
     <div class="d-flex">
 
-        <div>
+        <div class="title-desc">
             <div>
-                <h2>{{$single['title']}}</h2>
+                <h4 class="upbold">{{$single['title']}}</h4>
             </div>
 
-            <div class="d-flex">
-                <div class="d-flex">
-                    <p>U.S. Price: {{$single['price']}}</p>
-                    <p>Avaiable</p>
+            <div class="green-cont">
+                <div class="avaible col-7">
+                    <span class="upbold">U.S. Price: <span class="text-white">{{$single['price']}}</span></span>
+                    <span class="upbold">Avaiable</span>
                 </div>
-                <div>
-                    <p>Check Availability</p>
+                <div class="check-avaible col-3">
+                    <span class="text-white">Check Availability</span>
                 </div>
             </div>
             
-            <div>
+            <div class="col-10">
                 <p>{{$single['description']}}</p>
             </div>
         </div>
         
-        <div>
-            <p>Advertisement</p>
+        <div class="advertisement">
+            <span>Advertisement</span>
             <img src="{{Vite::asset('resources/images/adv.jpg')}}" alt="">
         </div>
     </div>
 
-        <div class="talentspecs d-flex">
-            <div class="talet">
+        <div class="talentspecs">
+            <div class="talent col-5">
                 <h2>Talent</h2>
                 <p>Art by: <a href="#"> {{implode(', ', $single['artists'])}} </a></p>
                 <p>Written by: <a href="#"> {{implode(', ', $single['writers'])}} </a></p>
             </div>
-            <div>
+            <div class="specs col-5">
                 <h2>Specs</h2>
                 <p>Series: {{$single['series']}}</p>
                 <p>U.S. Price: {{$single['price']}}</p>
